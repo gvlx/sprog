d920df2ebbcd7d3542e866c5fd9e79a63bd6ef83
grantm <grantm@ea9414c2-3f0f-0410-abdd-97b3e476b73c>
2005-02-28 18:16:20 +0000

    - dummy unregister_data_provider()

--- trunk/sprog/t/lib/DummyMachine.pm
+++ trunk/sprog/t/lib/DummyMachine.pm
@@ -23,8 +23,9 @@ sub new {
 }
 
 
-sub enable_idle_handler    { 1; }
-sub register_data_provider { 1; }
+sub enable_idle_handler      { 1; }
+sub register_data_provider   { 1; }
+sub unregister_data_provider { 1; }
 
 
 1;
