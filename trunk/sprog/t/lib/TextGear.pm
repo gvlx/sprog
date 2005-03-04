883d263030bca8e6cece324fef0dbba480cbbdc9
grantm <grantm@ea9414c2-3f0f-0410-abdd-97b3e476b73c>
2005-03-04 20:39:44 +0000

    - reset collected data on each run

--- trunk/sprog/t/lib/TextGear.pm
+++ trunk/sprog/t/lib/TextGear.pm
@@ -8,7 +8,17 @@ __PACKAGE__->mk_accessors(qw(
   text
 ));
 
+
+sub prime {
+  my $self = shift;
+
+  $self->text('');
+  $self->SUPER::prime();
+}
+
+
 sub has_input { 1; }
+
 
 sub data {
   my($self, $data) = @_;
