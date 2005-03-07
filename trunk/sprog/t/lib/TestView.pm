b33e85034e5c252ca5561f390f18839549dc439f
grantm <grantm@ea9414c2-3f0f-0410-abdd-97b3e476b73c>
2005-03-07 08:58:47 +0000

    - added methods for finding windows and buttons

--- trunk/sprog/t/lib/TestView.pm
+++ trunk/sprog/t/lib/TestView.pm
@@ -14,5 +14,34 @@ sub activate_menu_item {
 }
 
 
+sub find_window {
+  my($self, $name) = @_;
+
+  foreach my $window (Gtk2::Window->list_toplevels) {
+    my $title = $window->get_title or next;
+    return $window if($title =~ /$name/i);
+  }
+
+  return;
+}
+
+
+sub find_button {
+  my($self, $widget, $name) = @_;
+
+  return $widget
+    if($widget->isa('Gtk2::Button')  and  $widget->get_label =~ /$name/i);
+
+  if($widget->can('get_children')) {
+    foreach ($widget->get_children) {
+      my $button = $self->find_button($_, $name);
+      return $button if($button);
+    }
+  }
+
+  return;
+}
+
+
 1;
 
