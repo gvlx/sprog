512511efa3e48e9ffa7323d6ee2c41181ac1d225
grantm <grantm@ea9414c2-3f0f-0410-abdd-97b3e476b73c>
2005-03-08 10:47:41 +0000

    - add method for walking widget tree

--- trunk/sprog/t/lib/TestView.pm
+++ trunk/sprog/t/lib/TestView.pm
@@ -2,6 +2,9 @@ package TestView;
 
 use base qw(Sprog::GtkView);
 
+use strict;
+use warnings;
+
 
 sub activate_menu_item {
   my($self, $path) = @_;
@@ -30,7 +33,10 @@ sub find_button {
   my($self, $widget, $name) = @_;
 
   return $widget
-    if($widget->isa('Gtk2::Button')  and  $widget->get_label =~ /$name/i);
+    if($widget->isa('Gtk2::Button') 
+       and  $widget->get_label
+       and  $widget->get_label =~ /$name/i
+      );
 
   if($widget->can('get_children')) {
     foreach ($widget->get_children) {
@@ -40,6 +46,39 @@ sub find_button {
   }
 
   return;
+}
+
+
+sub get_tool_buttons {
+  my($self) = @_;
+
+  my(@buttons, %map);
+
+  $self->walk_children(
+    $self->toolbar->widget,
+    sub { $_ = shift; push @buttons, $_ if $_->isa('Gtk2::Button'); }
+  );
+
+  foreach my $b (@buttons) {
+    my $k = '';
+    $self->walk_children($b, 
+      sub { $k = $_[0]->get_label if $_[0]->isa('Gtk2::Label'); } 
+    );
+    $map{$k} = $b;
+  }
+
+  return %map;
+}
+
+
+sub walk_children {
+  my($self, $widget, $callback) = @_;
+
+  $callback->($widget);
+
+  if($widget->can('get_children')) {
+    $self->walk_children($_, $callback) foreach ($widget->get_children);
+  }
 }
 
 
