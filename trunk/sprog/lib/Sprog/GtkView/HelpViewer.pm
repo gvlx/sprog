3c6f2bc934f26322fcb96f70b1f05454027d52d1
grantm <grantm@ea9414c2-3f0f-0410-abdd-97b3e476b73c>
2005-03-24 10:08:51 +0000

    - support more styles

--- trunk/sprog/lib/Sprog/GtkView/HelpViewer.pm
+++ trunk/sprog/lib/Sprog/GtkView/HelpViewer.pm
@@ -70,14 +70,14 @@ sub _init_tags {
     weight             => PANGO_WEIGHT_BOLD,
     size               => $size * 1.6,
     pixels_above_lines => 12,
-    pixels_below_lines => 2,
+    pixels_below_lines => 4,
   );
 
   $buffer->create_tag('head2',
     family             => 'Sans',
     weight             => PANGO_WEIGHT_BOLD,
     size               => $size * 1.4,
-    pixels_above_lines => 6,
+    pixels_above_lines => 4,
     pixels_below_lines => 2,
   );
 
@@ -97,10 +97,38 @@ sub _init_tags {
     pixels_below_lines => 2,
   );
 
+  $buffer->create_tag('para',
+#    pixels_above_lines => 2,
+    pixels_below_lines => 8,
+  );
+
+  $buffer->create_tag('bullet',
+    left_margin        => 20,
+    pixels_above_lines => 2,
+    pixels_below_lines => 2,
+  );
+
   $buffer->create_tag('verbatim',
     wrap_mode          => 'none',
     family             => 'monospace',
-    size               => 11 * PANGO_SCALE,
+    size               => $size,
+  );
+
+  $buffer->create_tag('bold',
+    weight             => PANGO_WEIGHT_BOLD,
+  );
+
+  $buffer->create_tag('italic',
+    style              => 'italic'
+  );
+
+  $buffer->create_tag('code',
+    family             => 'monospace',
+  );
+
+  $buffer->create_tag('link',
+    underline          => 'single',
+    foreground         => 'blue',
   );
 
 }
@@ -165,17 +193,23 @@ sub on_home_activated {
 
 ########################### POD Event Handlers ###############################
 
-sub handle_text { $_[0]->_emit($_[1]); }
+sub handle_text       { $_[0]->_emit($_[1]); }
 
-sub start_head1 { $_[0]->_start_block(qw(head1)); }
-sub start_head2 { $_[0]->_start_block(qw(head2)); }
-sub start_head3 { $_[0]->_start_block(qw(head3)); }
-sub start_head4 { $_[0]->_start_block(qw(head4)); }
-sub start_Para  { $_[0]->_start_block; }
+sub start_head1       { $_[0]->_start_block(qw(head1)); }
+sub start_head2       { $_[0]->_start_block(qw(head2)); }
+sub start_head3       { $_[0]->_start_block(qw(head3)); }
+sub start_head4       { $_[0]->_start_block(qw(head4)); }
+sub start_Para        { $_[0]->_start_block(qw(para)); }
 sub start_Verbatim    { $_[0]->_start_block(qw(verbatim)); }
-sub start_item_bullet { $_[0]->_start_block; }
 sub start_item_number { $_[0]->_start_block; }
 sub start_item_text   { $_[0]->_start_block; }
+
+sub start_item_bullet {
+  my $self = shift;
+  
+  $self->_start_block(qw(bullet));
+  $self->_emit("\x{B7} ");
+}
 
 sub end_head1 { $_[0]->_end_block; }
 sub end_head2 { $_[0]->_end_block; }
@@ -186,6 +220,18 @@ sub end_Verbatim    { $_[0]->_end_block; }
 sub end_item_bullet { $_[0]->_end_block; }
 sub end_item_number { $_[0]->_end_block; }
 sub end_item_text   { $_[0]->_end_block; }
+
+sub start_B          { push @{$_[0]->{tag_stack}->[-1]}, 'bold'; }
+sub start_I          { push @{$_[0]->{tag_stack}->[-1]}, 'italic'; }
+sub start_C          { push @{$_[0]->{tag_stack}->[-1]}, 'code'; }
+sub start_F          { push @{$_[0]->{tag_stack}->[-1]}, 'code'; }
+sub start_L          { push @{$_[0]->{tag_stack}->[-1]}, 'link'; }
+
+sub end_B            { pop  @{$_[0]->{tag_stack}->[-1]}; }
+sub end_I            { pop  @{$_[0]->{tag_stack}->[-1]}; }
+sub end_C            { pop  @{$_[0]->{tag_stack}->[-1]}; }
+sub end_F            { pop  @{$_[0]->{tag_stack}->[-1]}; }
+sub end_L            { pop  @{$_[0]->{tag_stack}->[-1]}; }
 
 sub _start_block {
   my $self = shift;
@@ -220,9 +266,14 @@ PodTest - a script for testing POD parsing
 
 =head1 SYNOPSIS
 
-This is a paragraph
+This is a paragraph of meaningless text that is just meant to allow testing of
+word wrapping and inter-paragraph spacing.  If character markup worked, this
+word would be B<bold> and this one would be I<italics>.  The C<tail> command
+is not relevant.
+
+This is another paragraph
 (spread over multiple lines) that comes before a C<verbatim>
-block.
+block.  It's not as long as the first one.
 
   $this->isa('Verbatim::Block')
     && print $_;
@@ -252,7 +303,8 @@ two
 =item *
 
 three
-(the last one)
+(the last one and in fact the only one that is long enough to force wrapping
+over multiple lines so we can mess with indents)
 
 =back
 
@@ -262,3 +314,6 @@ Para before image.
 
 Para after image.
 
+=head3 See also
+
+See also L<Sprog>.
