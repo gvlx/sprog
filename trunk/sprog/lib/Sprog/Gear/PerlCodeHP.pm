1ebdf49bfff4db0154ea1c7e7c4c05b9b570c6ec
grantm <grantm@ea9414c2-3f0f-0410-abdd-97b3e476b73c>
2005-04-26 10:23:29 +0000

    - override 'print' to capture output to ptst

--- trunk/sprog/lib/Sprog/Gear/PerlCodeHP.pm
+++ trunk/sprog/lib/Sprog/Gear/PerlCodeHP.pm
@@ -2,13 +2,21 @@ package Sprog::Gear::PerlCodeHP;
 
 use strict;
 
+use Sprog::PrintToIt;
+
 use base qw(Sprog::Gear::PerlBase);
 
 sub input_type   { 'H'; }
 
 sub _sub_preamble {
-  return <<END_PERL;
-      my(\$self, \$r) = \@_;
+  return <<'END_PERL';
+      my($self, $r) = @_;
+
+      our %rec;
+      *rec = $r;
+
+      local(*STDOUT);
+      tie(*STDOUT, 'Sprog::PrintToIt');
 
       RECORD: {
 # line 1 "your code"
@@ -17,8 +25,8 @@ END_PERL
 
 
 sub _sub_postamble {
-  return <<END_PERL;
-        \$self->msg_out(data => \$_) if length;
+  return <<'END_PERL';
+        $self->msg_out(data => $_) if length;
       }
 END_PERL
 }
@@ -44,13 +52,13 @@ Sprog::Gear::PerlCode - execute your own Perl code for each input record
 
 =head1 DESCRIPTION
 
-WARNING: This gear is alpha code - the API is likely to change.
+WARNING: This gear is alpha code - the API may change.
 
 This gear allows the user to define a snippet of Perl code that will be
 executed for each input record.  The code snippet is wrapped with a small
 amount of boilerplate code:
 
-  $r = \%input_record;
+  $r = \%rec;
   $_ = '';
 
   RECORD: {
@@ -58,11 +66,17 @@ amount of boilerplate code:
     $self->msg_out(data => $_) if length;
   }
 
-Thus, the default behaviour is to make the input record available in C<$r> and
-to copy the resulting contents of $_ to the next downstream gear.  If nothing
-is assigned to C<$_> then no data will be passed to the next gear.  The code
-snippet can alter the contents of C<$_> and can use C<next> to shortcircuit the
-output message.
+Thus, the default behaviour is to make the input record available in the hash
+C<%rec> and the hashref C<$r> and to copy the resulting contents of $_ to the
+next downstream gear.  If nothing is assigned to C<$_> then no data will be
+passed to the next gear.  The code snippet can alter the contents of C<$_> and
+can use C<next> to short-circuit the output message.
+
+Note, the builtin C<print> function is overridden to assign its arguments to
+C<$_>.  So these two lines are equivalent:
+
+  print "$rec{host}\n";
+  $_  = "$rec{host}\n";
 
 =head1 COPYRIGHT 
 
