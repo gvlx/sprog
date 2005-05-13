fa5933c61d7868a8adf18d1e28a62240ae6d6485
grantm <grantm@ea9414c2-3f0f-0410-abdd-97b3e476b73c>
2005-05-13 10:20:17 +0000

    - add POD + help text

--- trunk/sprog/lib/Sprog/Gear/PerlCode.pm
+++ trunk/sprog/lib/Sprog/Gear/PerlCode.pm
@@ -20,8 +20,8 @@ sub prime {
 
 
 sub _sub_preamble {
-  return <<END_PERL;
-      my \$self = shift;
+  return <<'END_PERL';
+      my $self = shift;
 
       LINE: {
 # line 1 "your code"
@@ -30,11 +30,12 @@ END_PERL
 
 
 sub _sub_postamble {
-  return <<END_PERL;
-        print \$_;
+  return <<'END_PERL';
+        print $_;
       }
 END_PERL
 }
+
 
 sub line {
   my $self  = shift;
@@ -72,23 +73,21 @@ executed for each line of input.  The code snippet is wrapped with a small
 amount of boilerplate code (similar in purpose to Perl's C<-p> option):
 
   $_ = $input_line;
-
   LINE: {
-    # Perl snippet here
-    $self->msg_out(data => $_);
+    # Perl snippet inserted here
+    print $_;
   }
+
+The built-in C<print> function is also overridden to pass its arguments to the
+next downstream gear as a C<data> message.
 
 Thus, the default behaviour is to take the line of input in C<$_> and pass
 it directly to the next downstream gear.  The code snippet can alter the
-contents of C<$_> and can use C<next> to prevent the line being passed to
-the next gear.
+contents of C<$_>.  It can also call C<print> as many times as it wants to generate
+additional lines of output.
 
-Note, the builtin C<print> function is overridden to assign its arguments to
-C<$_>.  So these two lines are equivalent:
-
-  print uc($_);
-  $_  = uc($_);
-
+I<Note: unlike Perl's -p option, the C<print> is not in a C<continue> block
+so if you call next, the print will be skipped>.
 
 =head1 COPYRIGHT 
 
@@ -97,6 +96,68 @@ Copyright 2004-2005 Grant McLean E<lt>grantm@cpan.orgE<gt>
 This library is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself. 
 
-=cut
 
+=head1 HELP VIEWER TEXT
 
+=for sprog-help-text
+
+=head1 Perl Code Gear
+
+The 'Perl Code' gear allows you to set up a small chunk of Perl code that will 
+get run for each line of input.
+
+The default behaviour (if you don't add any code) is to pass each line
+unaltered out through the pipe connector to the next downstream gear.
+
+If you do add some Perl code, that code can alter the data, print extra lines
+of data or skip the line to prevent it being passed on.
+
+=head2 Properties
+
+The Read File gear has only one property - the Perl code to be run.  Type it
+in or paste it from the clipboard.
+
+=head2 Perl Wrapper
+
+Your Perl code will be wrapped in something like this:
+
+  $_ = $input line;
+  LINE: {
+    # Your Perl snippet here
+    print $_;
+  }
+
+Perl's built-in C<print> function is overridden in this context to pass the
+data down to the next gear.
+
+When your code snippet executes, the line of input data will be in C<$_>.  You
+can use
+
+  next LINE;
+
+to skip to the next line without passing the current line down to the next
+gear.
+
+=head2 Perl Code Cookbook
+
+Obviously, if you don't know any Perl, this gear is going to be of limited use
+to you, but here are a few snippets that might give you an idea of the
+possibilities.
+
+Put the current date and time onto the start of each line:
+
+  print localtime() . ': ';
+
+Skip lines that don't start with 'From: ' and only pass the remainder of the
+line for those that do:
+
+  next LINE unless /^From: (.*)$/;
+  $_ = "$1\n";
+
+=head2 Further Reading
+
+If you're new to Perl, the L<perlintro> manual page might be helpful.  Also the
+list of L<built-in functions|perlfunc> will give you some ideas.
+
+Both of the documents are likely to be a bit intimidating for non-programmers,
+however there are many good introductory books available.
--- trunk/sprog/lib/Sprog/Gear/UpperCase.pm
+++ trunk/sprog/lib/Sprog/Gear/UpperCase.pm
@@ -16,3 +16,36 @@ sub data {
 }
 
 1;
+
+__END__
+
+
+=head1 NAME
+
+Sprog::Gear::Uppercase - convert text to uppercase
+
+=head1 DESCRIPTION
+
+Any text passed through this gear will be converted to upper case.
+
+=head1 COPYRIGHT 
+
+Copyright 2004-2005 Grant McLean E<lt>grantm@cpan.orgE<gt>
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself. 
+
+
+=head1 HELP VIEWER TEXT
+
+=for sprog-help-text
+
+=head1 Uppercase File Gear
+
+This gear converts all text passed through it, to uppercase.  Both input and
+output use 'pipe' connectors.
+
+=head2 Properties
+
+The Uppercase gear has no properties.
+
