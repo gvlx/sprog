622e7c756b774778a9d2c782bf8c008a7bed3ba3
grantm <grantm@ea9414c2-3f0f-0410-abdd-97b3e476b73c>
2005-05-18 08:59:56 +0000

    - add POD

--- trunk/sprog/lib/Sprog/Gear/OutputToFH.pm
+++ trunk/sprog/lib/Sprog/Gear/OutputToFH.pm
@@ -45,3 +45,54 @@ sub _can_write {
 
 1;
 
+
+=head1 NAME
+
+Sprog::Gear::OutputToFH - a 'mixin' class for gears writing output to a file handle
+
+=head1 SYNOPSIS
+
+  use base qw(
+    Sprog::Gear
+    Sprog::Gear::OutputToFH
+  );
+
+  sub prime {
+    my($self) = @_;
+
+    my $fh = $self->_open_output_file or return;
+    $self->fh_out($fh);
+
+    return $self->SUPER::prime;
+  }
+
+
+=head1 DESCRIPTION
+
+This mixin is for use by gears which need to write to a file handle (ie: files,
+pipes, sockets ...).  It provides the appropriate links into the event loop to
+write using non-blocking IO.
+
+
+=head1 METHODS
+
+=head2 fh_out ( filehandle )
+
+The gear class is responsible for opening the file handle and then passing it
+to this method to make it available to the other methods in this class.
+
+=head2 data ( data )
+
+When the gear receives a C<data> message, this method will be invoked and the
+data will be queued to be written to the filehandle.
+
+=head1 COPYRIGHT 
+
+Copyright 2005 Grant McLean E<lt>grantm@cpan.orgE<gt>
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself. 
+
+=cut
+
+
