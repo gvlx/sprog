94fd9a3e59e8347dac2f26a7770b7735b223a9f0
grantm <grantm@ea9414c2-3f0f-0410-abdd-97b3e476b73c>
2005-06-25 09:27:23 +0000

    - exclude nav.html

--- trunk/sprog-web/publish.sh
+++ trunk/sprog-web/publish.sh
@@ -3,7 +3,7 @@
 cd html
 
 echo "Building archive"
-tar cfj /tmp/sprog-web.tbz --exclude=CVS --exclude=.cvsignore --exclude=.*.swp .
+tar cfj /tmp/sprog-web.tbz --exclude=CVS --exclude=.cvsignore --exclude=nav.html --exclude=.*.swp .
 
 echo "Uploading to SourceForge"
 ssh grantm@shell.sourceforge.net sh -c \
