4c05eea69d57ee092c396551c8f9a94d9be07791
grantm <grantm@ea9414c2-3f0f-0410-abdd-97b3e476b73c>
2005-12-03 09:39:11 +0000

    - us rsync to publish

--- trunk/sprog-web/publish.sh
+++ trunk/sprog-web/publish.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-cd html
-
-echo "Building archive"
-tar cfj /tmp/sprog-web.tbz --exclude=CVS --exclude=.cvsignore --exclude=nav.html --exclude=.*.swp .
+#cd html
+#
+#echo "Building archive"
+#tar cfj /tmp/sprog-web.tbz --exclude=CVS --exclude=.cvsignore --exclude=nav.html --exclude=.*.swp .
+#
+#echo "Uploading to SourceForge"
+#ssh grantm@shell.sourceforge.net sh -c \
+#  "'cd /home/groups/s/sp/sprog/htdocs/; tar xvfj -'" < /tmp/sprog-web.tbz
+#
+#rm -f /tmp/sprog-web.tbz
 
 echo "Uploading to SourceForge"
-ssh grantm@shell.sourceforge.net sh -c \
-  "'cd /home/groups/s/sp/sprog/htdocs/; tar xvfj -'" < /tmp/sprog-web.tbz
 
-rm -f /tmp/sprog-web.tbz
+RSYNCOPTS="--verbose --recursive --compress --checksum --links --perms --times --delete --delete-after --delay-updates --itemize-changes"
 
-echo "wget -C off -O - http://sprog.sourceforge.net/debian/dists/unstable/contrib/binary-i386/Packages.gz | gunzip | grep ^Version"
+rsync $RSYNCOPTS ./docroot/. grantm@shell.sourceforge.net:/home/groups/s/sp/sprog/htdocs/
 
-wget -C off -O - http://sprog.sourceforge.net/debian/dists/unstable/contrib/binary-i386/Packages.gz | gunzip | grep ^Version
+echo "wget -q --no-cache -O - http://sprog.sourceforge.net/debian/dists/unstable/contrib/binary-i386/Packages.gz | gunzip | grep ^Version"
+
+wget -q --no-cache -O - http://sprog.sourceforge.net/debian/dists/unstable/contrib/binary-i386/Packages.gz | gunzip | grep ^Version
