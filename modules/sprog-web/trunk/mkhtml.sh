dd342c0cefd0f9ec310f07934bc3463f63c41f55
grantm <grantm@ea9414c2-3f0f-0410-abdd-97b3e476b73c>
2006-03-21 08:48:01 +0000

    - set things up for publishing from svn tree

--- modules/sprog-web/trunk/mkhtml.sh
+++ modules/sprog-web/trunk/mkhtml.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+#
+# Use 'ttree' utility from the Perl Template Toolkit distribution to create
+# web site under 'docroot' directory from templates under 'html'.
+#
+
+cd `dirname $0`
 
 ttree -f .ttreerc
 
